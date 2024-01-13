f2aa07c54fc7b04fd55e4f059645055bf4f06297
sbslim <sbslim.dev@gmail.com>
2024-01-13 17:59:02 +0100

    feat: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,25 +2,159 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    'plugin:react/recommended',
+    'plugin:jsdoc/recommended',
+    'plugin:tsdoc/recommended',
+    'plugin:prefer-arrow-functions/recommended',
+    'eslint-config-prettier'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_module'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "node_module"],
+  parser: "@typescript-eslint/parser",
+  plugins: ['react',
+  'react-refresh',
+  '@typescript-eslint',
+  'jsx-a11y',
+  'jsdoc',
+  'tsdoc',
+  'prefer-arrow-functions',],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    "react/prefer-stateless-function": "error",
+    "react/button-has-type": "error",
+    "react/no-unused-prop-types": "error",
+    "react/jsx-pascal-case": "error",
+    "react/jsx-no-script-url": "error",
     "react/no-children-prop": "error",
-    "arrow-spacing": "error",
-    "no-confusing-arrow": "error",
+    "react/no-danger": "error",
+    "react/no-danger-with-children": "error",
+    "react/no-unstable-nested-components": ["error", { allowAsProps: true }],
+    "react/jsx-fragments": "error",
+    "react/destructuring-assignment": ["error", "always", { destructureInSignature: "always" }],
+    "react/jsx-no-leaked-render": ["error", { validStrategies: ["ternary"] }],
+    "react/jsx-max-depth": ["error", { max: 5 }],
+    "react/function-component-definition": ["warn", { namedComponents: "arrow-function" }],
+    "react/jsx-key": [
+      "error",
+      {
+        checkFragmentShorthand: true,
+        checkKeyMustBeforeSpread: true,
+        warnOnDuplicates: true,
+      },
+    ],
+    "react/jsx-no-useless-fragment": "warn",
+    "react/jsx-curly-brace-presence": "warn",
+    "react/no-typos": "warn",
+    "react/display-name": ["off", { ignoreTranspilerName: false }],
+    "react/self-closing-comp": "warn",
+    "react/jsx-sort-props": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/prop-types": "off",
     "no-duplicate-imports": "error",
     "no-var": "error",
     "object-shorthand": "off",
     "prefer-const": "error",
     "prefer-template": "warn",
+
+    // React Refresh
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+
+    // Naming convention
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      {
+        selector: "default",
+        format: ["camelCase"],
+        leadingUnderscore: "allow",
+      },
+      {
+        selector: "variable",
+        // Specify PascalCase for React components
+        format: ["PascalCase", "camelCase"],
+        leadingUnderscore: "allow",
+      },
+      {
+        selector: "parameter",
+        format: ["camelCase"],
+        leadingUnderscore: "allow",
+      },
+      {
+        selector: "property",
+        format: null,
+        leadingUnderscore: "allow",
+      },
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+    ],
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "filename-rules/match": [2, { ".ts": "camelcase", ".tsx": "pascalcase" }],
+    "import/no-default-export": "error",
+    "jsx-a11y/no-autofocus": [
+      2,
+      {
+        ignoreNonDOM: true,
+      },
+    ],
+
+    // TsDoc
+    "jsdoc/require-throws": "error",
+    "jsdoc/check-indentation": "warn",
+    "jsdoc/no-blank-blocks": "warn",
+    "jsdoc/require-asterisk-prefix": "warn",
+    "jsdoc/require-description": "warn",
+    "jsdoc/sort-tags": "warn",
+    "jsdoc/check-syntax": "warn",
+    "jsdoc/tag-lines": ["warn", "never", { startLines: 1 }],
+    "jsdoc/require-param": ["warn", { checkDestructuredRoots: false }],
+    "jsdoc/require-jsdoc": [
+      "warn",
+      {
+        publicOnly: true,
+        require: {
+          FunctionDeclaration: true,
+          FunctionExpression: true,
+          ArrowFunctionExpression: true,
+          ClassDeclaration: true,
+          ClassExpression: true,
+          MethodDefinition: true,
+        },
+        contexts: [
+          "VariableDeclaration",
+          "TSTypeAliasDeclaration",
+          // Encourage documenting React prop types
+          "TSPropertySignature",
+        ],
+        enableFixer: true,
+      },
+    ],
+    // tsdoc checks this syntax instead
+    "jsdoc/require-hyphen-before-param-description": "off",
+    "jsdoc/require-returns": "off",
+
+    "tsdoc/syntax": "warn",
+
+    // Arrow functions
+    "prefer-arrow-functions/prefer-arrow-functions": [
+  "warn",
+  {
+    classPropertiesAllowed: true,
+    disallowPrototype: true,
+    returnStyle: "unchanged",
   },
-}
+    ],
+    "arrow-body-style": "warn",
+    "prefer-arrow-callback": [
+      "warn",
+      {
+        allowNamedFunctions: true,
+      },
+    ],
+    "arrow-spacing": "error",
+    "no-confusing-arrow": "error",
+      },
+};
