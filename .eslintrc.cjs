5a0962cedaec6b52d3140c9de2f3cc027fe2cbf5
sbslim <sbslim.dev@gmail.com>
2024-01-14 14:54:45 +0100

    feat: doc eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,19 +21,20 @@ module.exports = {
     "filename-rules",
   ],
   rules: {
-    "react/prefer-stateless-function": "error",
-    "react/no-unused-prop-types": "error",
-    "react/jsx-pascal-case": "error",
-    "react/jsx-no-script-url": "error",
-    "react/no-children-prop": "error",
-    "react/no-danger": "error",
-    "react/no-danger-with-children": "error",
-    "react/no-unstable-nested-components": ["error", { allowAsProps: true }],
-    "react/jsx-fragments": "error",
-    "react/destructuring-assignment": ["error", "always", { destructureInSignature: "always" }],
-    "react/jsx-no-leaked-render": ["error", { validStrategies: ["ternary"] }],
-    "react/jsx-max-depth": ["error", { max: 5 }],
-    "react/function-component-definition": ["warn", { namedComponents: "arrow-function" }],
+    // General JavaScript/TypeScript Rules:
+    "react/prefer-stateless-function": "error", // Enforces the use of stateless functional components where possible.
+    "react/no-unused-prop-types": "error", // Reports unused PropTypes.
+    "react/jsx-pascal-case": "error", // Enforces PascalCase for user-defined JSX components.
+    "react/jsx-no-script-url": "error", // Disallows using javascript: URLs in JSX.
+    "react/no-children-prop": "error", // Disallows passing children as a prop.
+    "react/no-danger": "error", // Disallows using dangerouslySetInnerHTML.
+    "react/no-danger-with-children": "error", // Disallows using dangerouslySetInnerHTML when children are present.
+    "react/no-unstable-nested-components": ["error", { allowAsProps: true }], // Disallows the use of unstable nested components.
+    "react/jsx-fragments": "error", // Enforces the use of fragments (<></>) instead of <React.Fragment>.
+    "react/destructuring-assignment": ["error", "always", { destructureInSignature: "always" }], // Enforces destructuring assignment in certain situations.
+    "react/jsx-no-leaked-render": ["error", { validStrategies: ["ternary"] }], //  Disallows leaking of rendered values outside of the JSX scope.
+    "react/jsx-max-depth": ["error", { max: 5 }], // Enforces a maximum JSX depth.
+    "react/function-component-definition": ["warn", { namedComponents: "arrow-function" }], // Warns about using arrow functions for function component definitions.
     "react/jsx-key": [
       "error",
       {
@@ -41,26 +42,26 @@ module.exports = {
         checkKeyMustBeforeSpread: true,
         warnOnDuplicates: true,
       },
-    ],
-    "react/jsx-no-useless-fragment": "warn",
-    "react/jsx-curly-brace-presence": "warn",
-    "react/no-typos": "warn",
-    "react/display-name": ["off", { ignoreTranspilerName: false }],
-    "react/self-closing-comp": "warn",
-    "react/jsx-sort-props": "warn",
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-one-expression-per-line": "off",
-    "react/prop-types": "off",
-    "no-duplicate-imports": "error",
-    "no-var": "error",
-    "object-shorthand": "off",
-    "prefer-const": "error",
-    "prefer-template": "warn",
+    ], // Enforces consistent usage of keys in JSX elements.
+    "react/jsx-no-useless-fragment": "warn", // Warns about unnecessary usage of fragments.
+    "react/jsx-curly-brace-presence": "warn", // Warns about the presence of curly braces in JSX attributes.
+    "react/no-typos": "warn", // Warns about common typos in React component names.
+    "react/display-name": ["off", { ignoreTranspilerName: false }], //  disallows setting a display name for React components.
+    "react/self-closing-comp": "warn", //  Warns about not using self-closing tags for components without children.
+    "react/jsx-sort-props": "warn", // Warns about unsorted props in JSX elements.
+    "react/react-in-jsx-scope": "off", // Disallows the import of React when using JSX.
+    "react/jsx-one-expression-per-line": "off", // Disallows multiple expressions in a single JSX element.
+    "react/prop-types": "off", // Disables the requirement for PropTypes.
+    "no-duplicate-imports": "error", //  Disallows duplicate import statements.
+    "no-var": "error", //  Disallows the use of var for variable declarations.
+    "object-shorthand": "off", // Allows or disallows shorthand syntax for object properties.
+    "prefer-const": "error", // Enforces the use of const for variables that are never reassigned.
+    "prefer-template": "warn", // : Warns about using string concatenation instead of template literals.
 
-    // React Refresh
-    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    // React Refresh Rules:
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }], // Warns about exporting only React components when using React Refresh.
 
-    // Naming convention
+    // Naming Convention Rules:
     "@typescript-eslint/naming-convention": [
       "warn",
       {
@@ -88,26 +89,26 @@ module.exports = {
         selector: "typeLike",
         format: ["PascalCase"],
       },
-    ],
-    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
-    "filename-rules/match": [2, { ".ts": "camelcase", ".tsx": "pascalcase" }],
+    ], // Enforces a consistent naming convention for identifiers.
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }], //  Enforces the file extension for JSX files.
+    "filename-rules/match": [2, { ".ts": "camelcase", ".tsx": "pascalcase" }], // Enforces naming conventions for different file types (.ts and .tsx).
     "jsx-a11y/no-autofocus": [
       2,
       {
         ignoreNonDOM: true,
       },
-    ],
+    ], // Disallows using autofocus on elements.
 
-    // TsDoc
-    "jsdoc/require-throws": "error",
-    "jsdoc/check-indentation": "warn",
-    "jsdoc/no-blank-blocks": "warn",
-    "jsdoc/require-asterisk-prefix": "warn",
-    "jsdoc/require-description": "warn",
-    "jsdoc/sort-tags": "warn",
-    "jsdoc/check-syntax": "warn",
-    "jsdoc/tag-lines": ["warn", "never", { startLines: 1 }],
-    "jsdoc/require-param": ["warn", { checkDestructuredRoots: false }],
+    // TSDoc Rules:
+    "jsdoc/require-throws": "error", // Requires @throws tags in JSDoc comments for functions that throw errors.
+    "jsdoc/check-indentation": "warn", // Warns about incorrect indentation in JSDoc comments.
+    "jsdoc/no-blank-blocks": "warn", // Warns about blank lines in JSDoc blocks.
+    "jsdoc/require-asterisk-prefix": "warn", // Requires an asterisk at the beginning of every line in a JSDoc comment block.
+    "jsdoc/require-description": "warn", // Requires a description in JSDoc comments.
+    "jsdoc/sort-tags": "warn", // Warns if JSDoc tags are not sorted in a consistent order.
+    "jsdoc/check-syntax": "warn", // Warns about syntax errors in JSDoc comments.
+    "jsdoc/tag-lines": ["warn", "never", { startLines: 1 }], // Warns if there are blank lines before or after JSDoc tags.
+    "jsdoc/require-param": ["warn", { checkDestructuredRoots: false }], // Requires @param tags in JSDoc comments for function parameters.
     "jsdoc/require-jsdoc": [
       "warn",
       {
@@ -128,36 +129,34 @@ module.exports = {
         ],
         enableFixer: true,
       },
-    ],
-    // tsdoc checks this syntax instead
-    "jsdoc/require-hyphen-before-param-description": "off",
-    "jsdoc/require-returns": "off",
+    ], // Requires JSDoc comments for certain types of code elements.
+    "jsdoc/require-hyphen-before-param-description": "off", // Disabled. This rule is related to TSDoc and not applicable here.
+    "jsdoc/require-returns": "off", // Disabled. This rule is disabled in favor of the TSDoc @returns tag.
+    "tsdoc/syntax": "warn", //Warns about syntax errors in TSDoc comments.
 
-    "tsdoc/syntax": "warn",
-
-    // Arrow functions
+    // Arrow Function Rules:
     "prefer-arrow-functions/prefer-arrow-functions": [
       "warn",
       {
         classPropertiesAllowed: true,
         disallowPrototype: true,
       },
-    ],
-    "arrow-body-style": "warn",
+    ], // Enforces the use of arrow functions in certain contexts.
+    "arrow-body-style": "warn", //Warns about arrow functions that could be simplified.
     "prefer-arrow-callback": [
       "warn",
       {
         allowNamedFunctions: true,
       },
-    ],
-    "arrow-spacing": "error",
-    "no-confusing-arrow": "error",
+    ], // Warns about using non-arrow functions as callbacks.
+    "arrow-spacing": "error", // Enforces consistent spacing around the arrow in arrow functions.
+    "no-confusing-arrow": "error", // Disallows confusing arrow functions.
   },
   overrides: [
     {
       files: ["src/**/*.d.ts", "src/**/index.tsx", "src/main.tsx"],
       rules: {
-        "filename-rules/match": [2, { ".ts": "kebabcase", ".tsx": "kebabcase" }],
+        "filename-rules/match": [2, { ".ts": "kebabcase", ".tsx": "kebabcase" }], // Enforces kebab case for file names in specific file patterns.
       },
     },
   ],
